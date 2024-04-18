5b7ae27c04b1e93ac736c42fab760cbb0fda409e
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-04-19 03:41:26 +0600

    post api hitting

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+
+app.use(cors())
+
 const users = [
-    {id:1,name:'sagor',email:'sagor@gmail.com'},
-    {id:2,name:'mustakim',email:'mustakim@gmail.com'},
-    {id:3,name:'razu',email:'razu@gmail.com'}
+    { id: 1, name: 'sagor', email: 'sagor@gmail.com' },
+    { id: 2, name: 'mustakim', email: 'mustakim@gmail.com' },
+    { id: 3, name: 'razu', email: 'razu@gmail.com' }
 ]
 
-app.get('/users',(req , res)=>{
+app.get('/users', (req, res) => {
     res.send(users)
 })
 
+app.post('/users', (req, res) => {
+    console.log('post api hitting');
+    console.log(req.body)
+})
 
-app.get('/',(req , res)=>{
+
+app.get('/', (req, res) => {
     res.send('Users Management server is running')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running port ${port}`);
 })
